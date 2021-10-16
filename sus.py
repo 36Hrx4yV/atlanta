95e76022fefaf48c855bfff7844f0a52a465ec65
36Hrx4yV <88466366+36Hrx4yV@users.noreply.github.com>
2021-10-17 01:01:25 +0800

    Update sus.py

--- sus.py
+++ sus.py
@@ -1,3 +1,8 @@
-import zipfile
-with zipfile.ZipFile(lol.zip, 'r') as zip_ref:
-    zip_ref.extractall(app/)
+from zipfile import ZipFile 
+print("Enter file to unzip")
+file_name = input() 
+with ZipFile(file_name, 'r') as zip: 
+    zip.printdir() 
+    print('Extracting all the files now...') 
+    zip.extractall() 
+    print('Done!') 
